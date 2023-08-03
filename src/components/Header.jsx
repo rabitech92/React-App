f37d8a9eb5bbc950a39d7e19ee287e2b7116d0a9
Rayhanshuvo <106418651+rabitech92@users.noreply.github.com>
2023-08-03 15:55:02 +0600

    NavbarClear

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,11 +11,22 @@ export function Header(props) {
         <div>
             <nav class="navbar navbar-expand-lg navbar-dark bg-primary ">
                 <div className="container-fluid">
-                    <a className="navbar-brand ">Full Stack Application</a>      
-                    <button className="btn btn-light"><Link to={"/home"}>User List</Link></button>
-                    <button className="btn btn-light"><Link to={"/user"}>New User</Link></button>                         
-                    
-                </div>                
+                    <a className="navbar-brand ">Full Stack Application</a>
+                    <form class="d-flex  m-2" role="search">
+                        <input class="form-control " type="search" placeholder="Search" aria-label="Search"/>
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        
+                    </form>
+                    <span>
+                    <select className="form-select-sm mb-3" >
+                            <option selected>Open this select menu</option>
+                            <option>Patient</option>
+                            <option>Doctor</option>
+                        </select>
+                        <button className="btn btn-light m-2"><Link to={"/home"}>User List</Link></button>
+                        <button className="btn btn-light "><Link to={"/user"}>New User</Link></button> 
+                    </span>      
+                 </div>                
             </nav>            
         </div>
     )
